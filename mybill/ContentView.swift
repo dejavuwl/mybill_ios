074a6f554ca2d25398ecbac340f580b7451b2a49
远风 <dejavuwl@gmail.com>
2021-07-17 12:05:03 +0800

    Initial Commit

---
+++ mybill/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mybill
+//
+//  Created by farwind on 2021/7/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mybill/mybillApp.swift
@@ -0,0 +1,17 @@
+//
+//  mybillApp.swift
+//  mybill
+//
+//  Created by farwind on 2021/7/17.
+//
+
+import SwiftUI
+
+@main
+struct mybillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
